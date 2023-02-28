46c32e67de3311ff7631a7ffc74415977603d952
4513ECHO <4513echo@gmail.com>
2023-03-01 00:42:47 +0900

    fix: Decode URI query

--- main.ts
+++ main.ts
@@ -78,13 +78,11 @@ wellKnown.get("/nodeinfo/2.0", (ctx) => {
 });
 wellKnown.get("/webfinger", (ctx) => {
   const { hostname } = new URL(ctx.req.url);
-  console.debug("debug", Deno.inspect(ctx.req.query(), { compact: true }));
-  const resource = ctx.req.query("resource");
+  const resource = decodeURIComponent(ctx.req.query("resource") ?? "");
   if (!resource || !resource.startsWith("acct:")) {
     return ctx.body(null, 400);
   }
   const handle = parseHandle(resource.substring("acct:".length));
-  console.debug("debug", Deno.inspect(handle, { compact: true }));
   if (!handle.domain) {
     return ctx.body(null, 400);
   } else if (handle.domain !== hostname) {
